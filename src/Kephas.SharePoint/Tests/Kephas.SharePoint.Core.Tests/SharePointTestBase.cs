e4191574b0783d0f7be4dd6430f2d14062e8b566
Ioan Crișan <ioan.crisan@kephas-software.com>
2019-11-12 14:49:35 +0200

    Added ITokenizer service

---
+++ src/Kephas.SharePoint/Tests/Kephas.SharePoint.Core.Tests/SharePointTestBase.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SharePointTestBase.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the KEPHAS license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the SharePoint test base class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.SharePoint.Core.Tests
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Kephas.SharePoint.Text;
+    using Kephas.Testing.Composition;
+
+    public abstract class SharePointTestBase : CompositionTestBase
+    {
+        public override IEnumerable<Assembly> GetDefaultConventionAssemblies()
+        {
+            return new List<Assembly>(base.GetDefaultConventionAssemblies()) {
+                typeof(ITokenizer).Assembly,            // Kephas.SharePoint.Core
+            };
+        }
+    }
+}
