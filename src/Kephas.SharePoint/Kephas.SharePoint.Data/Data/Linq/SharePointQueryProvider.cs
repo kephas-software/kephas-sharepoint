0e41f75b1e77b0b9d6e5ecdb1d1558d9f0e280ef
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-01-30 17:21:57 +0200

    Add async methods to SharePointQueryProvider

--- src/Kephas.SharePoint/Kephas.SharePoint.Data/Data/Linq/SharePointQueryProvider.cs
+++ src/Kephas.SharePoint/Kephas.SharePoint.Data/Data/Linq/SharePointQueryProvider.cs
@@ -14,7 +14,8 @@ namespace Kephas.SharePoint.Data.Linq
     using System.Linq;
     using System.Linq.Expressions;
     using System.Reflection;
-
+    using System.Threading;
+    using System.Threading.Tasks;
     using Kephas.Data;
     using Kephas.Data.Linq;
     using Kephas.Diagnostics.Contracts;
@@ -71,19 +72,25 @@ namespace Kephas.SharePoint.Data.Linq
         /// </value>
         public IListInfo ListInfo { get; }
 
-        private class InternalQueryProvider : IQueryProvider
+        public override async Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
         {
-            /// <summary>
-            /// The generic method of IQueryable.CreateQuery{TElement}.
-            /// </summary>
+            Requires.NotNull(expression, nameof(expression));
+
+            var internalQueryProvider = (InternalQueryProvider)this.NativeQueryProvider;
+            var executionResult = internalQueryProvider.Execute<TResult>(this.GetExecutableExpression(expression));
+            return this.AttachEntitiesToDataContext(executionResult);
+        }
+
+        private class InternalQueryProvider : IAsyncQueryProvider
+        {
             private static readonly MethodInfo CreateQueryMethod =
                 ReflectionHelper.GetGenericMethodOf(_ => ((IQueryProvider)null).CreateQuery<int>(null));
 
-            /// <summary>
-            /// The generic method of IQueryable.Execute{TResult}.
-            /// </summary>
             private static readonly MethodInfo ExecuteMethod =
                 ReflectionHelper.GetGenericMethodOf(_ => ((IQueryProvider)null).Execute<int>(null));
+
+            private static readonly MethodInfo ExecuteAsyncMethod =
+                ReflectionHelper.GetGenericMethodOf(_ => ((IAsyncQueryProvider)null).ExecuteAsync<int>(null, default));
 
             /// <summary>
             /// Gets or sets the provider.
@@ -149,15 +156,77 @@ namespace Kephas.SharePoint.Data.Linq
                 return execute.Call(this, expression);
             }
 
+            /// <summary>
+            /// Executes the query represented by a specified expression tree.
+            /// </summary>
+            /// <typeparam name="TResult">Type of the result.</typeparam>
+            /// <param name="expression">An expression tree that represents a LINQ query.</param>
+            /// <returns>
+            /// The value that results from executing the specified query.
+            /// </returns>
             public TResult Execute<TResult>(Expression expression)
             {
                 var listFullName = this.Provider.QueryOperationContext.ListFullName();
 
                 Requires.NotNull(listFullName, nameof(listFullName));
 
-                var list = this.Provider.SiteService.GetListAsync(listFullName).GetResultNonLocking();
+                var list = this.Provider.SiteService.GetList(listFullName);
                 var query = CamlQuery.CreateAllItemsQuery();
-                var listItems = this.Provider.SiteService.GetListItemsAsync(list, query).GetResultNonLocking();
+                var listItems = this.Provider.SiteService.GetListItems(list, query);
+                var entities = listItems.Select(item => new SharePointEntity(this.Provider.ListInfo, item)).ToList();
+
+                return (TResult)(object)entities;
+            }
+
+            /// <summary>
+            /// Asynchronously executes the query represented by a specified expression tree.
+            /// </summary>
+            /// <param name="expression">An expression tree that represents a LINQ query.</param>
+            /// <param name="cancellationToken">Optional. A
+            ///                                 <see cref="T:System.Threading.CancellationToken" /> to
+            ///                                 observe while waiting for the task to complete.</param>
+            /// <returns>
+            /// A task that represents the asynchronous operation. The task result contains the value that
+            /// results from executing the specified query.
+            /// </returns>
+            public async Task<object> ExecuteAsync(Expression expression, CancellationToken cancellationToken = default)
+            {
+                Requires.NotNull(expression, nameof(expression));
+
+                var expressionType = expression.Type;
+                var expressionElementType = expressionType.TryGetEnumerableItemType();
+                if (expressionElementType != null)
+                {
+                    expressionType = typeof(IEnumerable<>).MakeGenericType(expressionElementType);
+                }
+
+                var executeAsync = ExecuteAsyncMethod.MakeGenericMethod(expressionType);
+                var typedTask = (Task)executeAsync.Call(this, expression, cancellationToken);
+                await typedTask.PreserveThreadContext();
+                return typedTask.GetPropertyValue(nameof(Task<int>.Result));
+            }
+
+            /// <summary>
+            /// Asynchronously executes the strongly-typed query represented by a specified expression tree.
+            /// </summary>
+            /// <typeparam name="TResult">The type of the value that results from executing the query.</typeparam>
+            /// <param name="expression">An expression tree that represents a LINQ query.</param>
+            /// <param name="cancellationToken">Optional. A
+            ///                                 <see cref="T:System.Threading.CancellationToken" /> to
+            ///                                 observe while waiting for the task to complete.</param>
+            /// <returns>
+            /// A task that represents the asynchronous operation. The task result contains the value that
+            /// results from executing the specified query.
+            /// </returns>
+            public async Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
+            {
+                var listFullName = this.Provider.QueryOperationContext.ListFullName();
+
+                Requires.NotNull(listFullName, nameof(listFullName));
+
+                var list = await this.Provider.SiteService.GetListAsync(listFullName).PreserveThreadContext();
+                var query = CamlQuery.CreateAllItemsQuery();
+                var listItems = await this.Provider.SiteService.GetListItemsAsync(list, query).PreserveThreadContext();
                 var entities = listItems.Select(item => new SharePointEntity(this.Provider.ListInfo, item)).ToList();
 
                 return (TResult)(object)entities;
