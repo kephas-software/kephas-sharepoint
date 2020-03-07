1587f6730b3bd5785bee3649dc236df69753dfae
Ioan Crișan <ioan.crisan@kephas-software.com>
2020-03-07 19:38:36 +0200

    Added the support for .NET Core 3.1

---
+++ src/Kephas.SharePoint/Kephas.SharePoint.Client/ClientContext.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ClientContext.cs" company="Kephas Software SRL">
+//   Copyright (c) Kephas Software SRL. All rights reserved.
+//   Licensed under the KEPHAS license. See LICENSE file in the project root for full license information.
+// </copyright>
+// <summary>
+//   Implements the client context class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Kephas.SharePoint
+{
+    using Kephas.Composition;
+    using Kephas.Services;
+
+    /// <summary>
+    /// Provides the client context.
+    /// </summary>
+    public class ClientContext : Context
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientContext"/> class.
+        /// </summary>
+        /// <param name="compositionContext">Context for the composition.</param>
+        public ClientContext(ICompositionContext compositionContext)
+            : base(compositionContext)
+        {
+        }
+    }
+}
